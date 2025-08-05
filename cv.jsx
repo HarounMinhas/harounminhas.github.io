359179950585ffb5191f1675f4d8c21635eb34fc
Haroun Minhas <152099124+HarounMinhas@users.noreply.github.com>
2025-08-05 14:12:43 +0200

    Apply grey theme and populate pages

--- cv.jsx
+++ cv.jsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <nav className="navbar navbar-expand-lg fixed-top portfolio-navbar gradient navbar-dark">
         <div className="container">
-          <a className="navbar-brand logo" href="#">Brand</a>
+          <a className="navbar-brand logo" href="#">Haroun Minhas</a>
           <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span className="visually-hidden">Toggle navigation</span>
             <span className="navbar-toggler-icon"></span>
@@ -21,8 +21,33 @@ function App() {
       <main className="page" style={{marginTop: '56px'}}>
         <section className="portfolio-block cv">
           <div className="container">
-            <h2 className="mb-4">Curriculum Vitae</h2>
-            <p>Coming soon...</p>
+            <div className="row">
+              <div className="col-md-4 text-center">
+                <img
+                  className="img-fluid rounded mb-3"
+                  src="https://avatars.githubusercontent.com/u/152099124?v=4"
+                  alt="Haroun Minhas"
+                />
+                <p>Ostend, Belgium</p>
+                <p>
+                  <a href="https://github.com/HarounMinhas" target="_blank">github.com/HarounMinhas</a>
+                </p>
+              </div>
+              <div className="col-md-8">
+                <h2 className="mb-3">Profile</h2>
+                <p>
+                  Background in the social sector, strong in project management and
+                  communication. Now in IT, skilled in C# .NET and honing HTML, CSS,
+                  JavaScript and SQL skills.
+                </p>
+                <h3 className="mt-4">Skills</h3>
+                <ul>
+                  <li>Project management & communication</li>
+                  <li>C# .NET development</li>
+                  <li>HTML, CSS, JavaScript & SQL</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </section>
       </main>
