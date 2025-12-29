e5251cd311a086c081a4c9387cd01981e61fd598
Haroun Minhas <152099124+HarounMinhas@users.noreply.github.com>
2025-12-29 14:01:57 +0530

    Move ThemeSwitcher to navbar and AccessibilityControls inside menu

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import AccessibilityControls from './AccessibilityControls';
+import ThemeSwitcher from './ThemeSwitcher';
 
 const MainNavbar = () => {
   return (
@@ -10,8 +11,8 @@ const MainNavbar = () => {
         <Navbar.Brand as={Link} to="/" className="fw-semibold text-uppercase mb-0">
           Logopedie Marieke
         </Navbar.Brand>
-        <div className="d-flex align-items-center ms-auto gap-3">
-          <AccessibilityControls variant="navbar" />
+        <div className="d-flex align-items-center ms-auto gap-2">
+          <ThemeSwitcher />
           <Navbar.Toggle aria-controls="main-navbar" aria-label="Navigatie tonen" />
         </div>
         <Navbar.Collapse id="main-navbar" className="justify-content-end">
@@ -31,6 +32,12 @@ const MainNavbar = () => {
             <Nav.Link as={NavLink} to="/alternatieve-opmaak">
               Alternatieve opmaak
             </Nav.Link>
+            <div className="d-lg-none">
+              <hr className="my-2" />
+            </div>
+            <div className="ms-lg-3">
+              <AccessibilityControls variant="navbar" />
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
